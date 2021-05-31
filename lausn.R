9c709370530205cb04a32cce32847a6c5a2640c3
Kári Gautason <iskallinn@gmail.com>
2021-05-31 14:34:09 +0000

    Create lausn.R
    heildarlausn

---
+++ lausn.R
@@ -0,0 +1,15 @@
+ y =  data.table(
+ ID = c(1:3600000),
+ birthyear = c(runif(n=3600000, min = 1921, max = 2021))
+ )
+ # n segir til um hversu margir skammtar eru til dreifingar i vikunni
+ n <- 20000
+ urtak <- y[birthyear > 1975 & birthyear < 2005]
+ y <- y[sample(x=n),]
+ View(y)
+ y =  data.table(
+   ID = c(1:3600000),
+   birthyear = c(round(runif(n=3600000, min = 1921, max = 2021)))
+ )
+ urtak <- y[birthyear > 1975 & birthyear < 2005]
+ y <- y[sample(x=n),]
